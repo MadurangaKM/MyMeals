faf30826f336d2cfc6ca9fee7c2455d08caeeb06
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2022-06-01 17:22:31 +0530

    text input changes

--- MyMealsApp/common-components/TextInput.js
+++ MyMealsApp/common-components/TextInput.js
@@ -6,7 +6,11 @@ const TextInputField = (props) => {
   const mode = useSelector((state) => state.DarkLightModeChangerData.darkMode);
   const styles = StyleSheet.create({
     textInput: {
-      borderColor: props.error ? Colors.errorColor : Colors.borderColor,
+      borderColor: props.error
+        ? "red"
+        : mode
+        ? Colors.surfaceColor
+        : Colors.borderColor,
       borderWidth: 1,
       padding: 3,
       paddingLeft: 10,
@@ -33,7 +37,8 @@ const TextInputField = (props) => {
       <Text
         style={{
           marginTop: props.error ? 5 : 0,
-          color: Colors.errorColor,
+          marginBottom: props.error ? 10 : 0,
+          color: mode ? Colors.errorColorDarkMode : Colors.errorColor,
           ...GlobalStyle.BodyTwo,
         }}
       >
