7cd0bb8727a3c590c25fd93bc6c06470f8518544
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2022-06-08 17:56:16 +0530

    changes

--- MyMealsApp/common-components/PrimaryButton.js
+++ MyMealsApp/common-components/PrimaryButton.js
@@ -4,6 +4,20 @@ import { GlobalStyle } from "../constants/GlobleStyle";
 import { Ionicons } from "@expo/vector-icons";
 
 const Button = (props) => {
+  const styles = StyleSheet.create({
+    button: {
+      backgroundColor: props.isBorder ? "transparent" : Colors.primary,
+      height: 38,
+      borderRadius: 10,
+      alignItems: "center",
+      justifyContent: "center",
+      paddingHorizontal: 20,
+      flexDirection: "row",
+      borderColor: props.isBorder ? Colors.primaryDark : "transparent",
+      borderWidth: props.isBorder ? 1 : 0,
+      // borderStyle: props.isBorder ? "dashed" : "hidden",
+    },
+  });
   return (
     <TouchableOpacity
       onPress={props.onPress}
@@ -13,13 +27,17 @@ const Button = (props) => {
         <Ionicons
           name={props.iconName}
           size={18}
-          color={Colors.drakNormalTextColor}
+          color={
+            props.isBorder ? Colors.primaryDark : Colors.drakNormalTextColor
+          }
           style={props.iconOnly ? {} : { marginRight: 4 }}
         />
       )}
       <Text
         style={{
-          color: Colors.drakNormalTextColor,
+          color: props.isBorder
+            ? Colors.primaryDark
+            : Colors.drakNormalTextColor,
           paddingTop: Platform.OS === "android" ? 3 : 0,
           ...GlobalStyle.ButtonText,
         }}
@@ -29,16 +47,5 @@ const Button = (props) => {
     </TouchableOpacity>
   );
 };
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: Colors.primary,
-    height: 38,
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingHorizontal: 20,
-    flexDirection: "row",
-  },
-});
 
 export default Button;
