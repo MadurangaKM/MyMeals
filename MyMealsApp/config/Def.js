6546faf2b6a738206049403bb959656da5ca08a9
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2022-06-08 17:55:41 +0530

    Def added

---
+++ MyMealsApp/config/Def.js
@@ -0,0 +1,13 @@
+const Def = {
+  category: [
+    "Vegetables",
+    "Fruits",
+    "Meat",
+    "Dairy",
+    "Grains",
+    "Baked Goods",
+    "Seafood",
+    "Herbs and Spices",
+  ],
+};
+export default Def;
